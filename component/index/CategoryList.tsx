d34727a3bdfbea1df2a57f5b5961c641fe59a2d8
ekdlwp123 <ekdlwp123@gamil.com>
2022-04-13 16:45:36 +0900

    product category css

--- component/index/CategoryList.tsx
+++ component/index/CategoryList.tsx
@@ -1,28 +1,31 @@
 import { NextPage } from 'next'
+import Link from 'next/link'
 import { product } from '../../models/Product'
-import styles from '../../styles/CategoryList.module.css'
+import CategoryListStyle from '../../styles/CategoryList.module.css'
 
 const CategoryList: NextPage<{ data: product }> = ({ data }) => {
     return (
         <div>
-            <div className={styles.content}>
-                <div className={styles.tumbs}>
-                    <img className={styles.image} src={data.imageUrl}></img>
-                </div>
-                <div className={styles.itemInfo}>
-                    <div className={styles.infoStyle}>
-                        <span>{data.name}</span>
-                        <span>{data.category1} | {data.category2}</span>
-                        <div className={styles.btn_group}>
-                            <button>찜하기</button>
-                            <button>장바구니</button>
-                        </div>
+            <div className={CategoryListStyle.content}>
+                <Link href={`/product?no=${data.no}`} passHref>
+                    <div className={CategoryListStyle.tumbs}>
+                        <img className={CategoryListStyle.image} src={data.imageUrl}></img>
+                    </div>
+                </Link>
+                <div className={CategoryListStyle.itemInfo}>
+                    <Link href={`/product?no=${data.no}`} passHref>
+                        <span className={CategoryListStyle.product_name}>{data.name}</span>
+                    </Link>
+                    <span>{data.category1} | {data.category2}</span>
+                    <div className={CategoryListStyle.btn_group}>
+                        <button className={CategoryListStyle.like_button}>찜하기</button>
+                        <button className={CategoryListStyle.itembox_button}>장바구니</button>
                     </div>
                 </div>
-                <div className={styles.sideInfo}>
-                    <div className={styles.infoStyle}>{data.price}</div>
-                    <div className={styles.postStyle}>택배비3500원</div>
-                    <div className={styles.makerStyle}>{data.maker}</div>
+                <div className={CategoryListStyle.sideInfo}>
+                    <div><strong>{data.price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')}</strong>원</div>
+                    <span>배송비 3,000원</span>
+                    <span>{data.maker}</span>
                 </div>
             </div>
         </div >
