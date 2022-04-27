3fab83fb4881ee95dd4ac9916984c33f36d48207
ryun <ekdlwp123@gmail.com>
2022-04-27 10:20:26 +0900

    header id add

--- component/index/HeaderCompo.tsx
+++ component/index/HeaderCompo.tsx
@@ -3,21 +3,23 @@ import Link from 'next/link'
 import useCustomSWR from '../../utils/client/useCustumSWR'
 import MenuToggle from '../menutoggle/MenuToggle'
 import { NextPage } from 'next'
-import { Default, Moblie } from '../responsive'
 import customAxios from '../../utils/customAxios'
 import { useRouter } from 'next/router'
 import Search from '../Search'
 import Loading from '../Loading'
+import { useState } from 'react'
 
 
 const HeaderCompo: NextPage = () => {
     const router = useRouter()
-
-    const { data, isLoading, isApiError, isServerError } = useCustomSWR("/api/user/me", {}, true)
+    const params = new URLSearchParams();
+    ["id"].forEach(value => params.append("required", value))
+    const { data, isLoading, isApiError, isServerError } = useCustomSWR("/api/user/me", { params }, true)
     if (isLoading) return <div><Loading /></div>
     if (isServerError) {
         alert("서버 에러가 발생하였습니다")
     }
+
     const role = data?.role
     async function logout() {
         try {
@@ -34,145 +36,79 @@ const HeaderCompo: NextPage = () => {
     }
 
     return (
-        <>
-            <Default>
-                <div className={styles.header}>
-                    <div className={styles.headbar}>
+        <div className={styles.header}>
+            <div className={styles.headbar}>
 
-                        <div className={styles.category}>
-                            <MenuToggle />
-                            <Link href="/" passHref>
-                                <div className={styles.logo}></div>
-                            </Link>
-                        </div>
-                        <Search></Search>
-                        {
-                            isApiError
-                                ? <>
-                                    <div className={styles.logingroup}>
-                                        <Link href="/login" passHref>
-                                            <div className={styles.loginBtn}>로그인</div>
-                                        </Link>
-                                        <span>|</span>
-                                        <Link href="/signup" passHref>
-                                            <div className={styles.loginBtn}>회원가입</div>
-                                        </Link>
+                <div className={styles.category}>
+                    <MenuToggle />
+                    <Link href="/" passHref>
+                        <div className={styles.logo}></div>
+                    </Link>
+                </div>
+                <Search></Search>
+                {
+                    isApiError
+                        ? <>
+                            <div className={styles.logingroup}>
+                                <Link href="/login" passHref>
+                                    <div className={styles.loginBtn}>로그인</div>
+                                </Link>
+                                <span>|</span>
+                                <Link href="/signup" passHref>
+                                    <div className={styles.loginBtn}>회원가입</div>
+                                </Link>
+                            </div>
+                            <div className={styles.mypage}>
+                                <Link href="/mypage" passHref>
+                                    <div className={styles.mypagebtn}>
                                     </div>
-                                    <div className={styles.mypage}>
-                                        <Link href="/mypage" passHref>
-                                            <div className={styles.mypagebtn}>
-                                            </div>
-                                        </Link>
-                                        <div className={styles.itemBox}>
+                                </Link>
+                                <div className={styles.itemBox}>
+                                </div>
+                            </div>
+                        </>
+                        : role === "admin"
+                            ? <>
+                                <div className={styles.logingroup}>
+                                    <Link href="/admin" passHref>
+                                        <div>
+                                            관리자
                                         </div>
+                                    </Link>
+                                    <span>|</span>
+                                    <div className={styles.loginbtn} onClick={logout}>로그아웃</div>
+                                </div>
+                                <div className={styles.mypage}>
+                                    <Link href="/mypage" passHref>
+                                        <div className={styles.mypagebtn}>
+                                        </div>
+                                    </Link>
+                                    <div className={styles.itemBox}>
                                     </div>
-                                </>
-                                : role === "admin"
-                                    ? <>
-                                        <div className={styles.logingroup}>
-                                            <Link href="/admin" passHref>
-                                                <div>
-                                                    관리자
-                                                </div>
-                                            </Link>
-                                            <span>|</span>
-                                            <div className={styles.loginbtn} onClick={logout}>로그아웃</div>
+                                </div>
+                            </>
+                            : <>
+                                <div className={styles.logingroup}>
+                                    <Link href="/mypage" passHref>
+                                        <div className={styles.id}>
+                                            {data.id}
                                         </div>
-                                        <div className={styles.mypage}>
-                                            <Link href="/mypage" passHref>
-                                                <div className={styles.mypagebtn}>
-                                                </div>
-                                            </Link>
-                                            <div className={styles.itemBox}>
-                                            </div>
+                                    </Link>
+                                    <span>|</span>
+                                    <div className={styles.loginbtn} onClick={logout}>로그아웃</div>
+                                </div>
+                                <div className={styles.mypage}>
+                                    <Link href="/mypage" passHref>
+                                        <div className={styles.mypagebtn}>
                                         </div>
-                                    </>
-                                    : <>
-                                        <div className={styles.logingroup}>
-                                            <div className={styles.loginbtn} onClick={logout}>로그아웃</div>
-                                        </div>
-                                        <div className={styles.mypage}>
-                                            <Link href="/mypage" passHref>
-                                                <div className={styles.mypagebtn}>
-                                                </div>
-                                            </Link>
-                                            <div className={styles.itemBox}>
-                                            </div>
-                                        </div>
-                                    </>
-                        }
-                    </div>
-                </div >
-            </Default>
-            <Moblie>
-                <div className={styles.header}>
-                    <div className={styles.headbarMoblie}>
-                        <div className={styles.categoryMobile}>
-                            <MenuToggle />
-
-                        </div>
-                        <Link href="/" passHref>
-                            <div className={styles.logoMobile}></div>
-                        </Link>
-                        {
-                            isApiError ?
-                                <>
-                                    <div className={styles.logingroupMobile}>
-                                        <Link href="/login" passHref>
-                                            <div className={styles.loginBtn}>로그인</div>
-                                        </Link>
-                                        <span>|</span>
-                                        <Link href="/signup" passHref>
-                                            <div className={styles.loginBtn}>회원가입</div>
-                                        </Link>
+                                    </Link>
+                                    <div className={styles.itemBox}>
                                     </div>
-                                    <div className={styles.mypageMobile}>
-                                        <Link href="/mypage" passHref>
-                                            <div className={styles.mypagebtnMoblie}>
-                                            </div>
-                                        </Link>
-                                        <div className={styles.itemBoxMoblie}>
-                                        </div>
-                                    </div>
-                                </> : role === "admin" ?
-                                    <>
-                                        <div className={styles.logingroupMobile}>
-                                            <Link href="/admin" passHref>
-                                                <div>
-                                                    관리자
-                                                </div>
-                                            </Link>
-                                            <span>|</span>
-                                            <div className={styles.loginbtn} onClick={logout}>로그아웃</div>
-                                        </div>
-                                        <div className={styles.mypage}>
-                                            <Link href="/mypage" passHref>
-                                                <div className={styles.mypagebtnMoblie}>
-                                                </div>
-                                            </Link>
-                                            <div className={styles.itemBoxMoblie}>
-                                            </div>
-                                        </div>
-                                    </> :
-                                    <>
-                                        <div className={styles.logingroupMobile}>
-                                            <div className={styles.loginbtn} onClick={logout}>로그아웃</div>
-                                        </div>
-                                        <div className={styles.mypage}>
-                                            <Link href="/mypage" passHref>
-                                                <div className={styles.mypagebtnMoblie}>
-                                                </div>
-                                            </Link>
-                                            <div className={styles.itemBoxMoblie}>
-                                            </div>
-                                        </div>
-                                    </>
-                        }
-                    </div>
-                </div >
-            </Moblie>
-        </>
-
+                                </div>
+                            </>
+                }
+            </div>
+        </div >
     )
 }
 
